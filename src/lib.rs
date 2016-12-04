e3b529c3a04b45ea88fd23193381d27c3d29c1f6
peltoche <dev@halium.fr>
2016-12-04 13:02:53 +0100

    Update travis.yml.

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,20 @@ pub mod line;
 #[cfg(any(feature = "ical-parser", feature = "vcard-parser"))]
 pub mod parser;
 
-
 pub const PARAM_VALUE_DELIMITER: char = ',';
 pub const VALUE_DELIMITER: char = ':';
 pub const PARAM_DELIMITER: char = ';';
 pub const PARAM_NAME_DELIMITER: char = '=';
 
+
 #[cfg(feature = "ical-parser")]
 pub use parser::ical::IcalParser;
+
 #[cfg(feature = "vcard-parser")]
 pub use parser::vcard::VcardParser;
+
 #[cfg(feature = "line-parser")]
 pub use line::parser::LineParser;
+
 #[cfg(feature = "line-reader")]
 pub use line::reader::LineReader;
